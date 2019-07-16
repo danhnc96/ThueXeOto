3d05c49076bef2db732b5beb3a65586eb4365281
baoari <48285385+baoari@users.noreply.github.com>
2019-07-16 14:25:54 +0700

    up

---
+++ QLThueXeOto/QLThueXeOto/Controllers/NguoiDungController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QLThueXeOto.Models;
+
+namespace QLThueXeOto.Controllers
+{
+    public class NguoiDungController : Controller
+    {
+        //
+        // GET: /NguoiDung/
+        QuanLyThueXeOtoEntities db = new QuanLyThueXeOtoEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult DangKy()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DangKy(Khach kh)
+        {
+            if(ModelState.IsValid){
+                //Chen du lieu vao bang khach
+                db.Khaches.Add(kh);
+                //Luu vso csdl
+                db.SaveChanges();
+            }
+            return View();
+        }
+        [HttpGet]
+        public ActionResult DangNhap()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DangNhap(FormCollection f)
+        {
+            string sTaiKhoan = f["txtTaiKhoan"].ToString();
+            string sMatKhau = f.Get("txtMatKhau").ToString();
+            Khach kh = db.Khaches.SingleOrDefault(n => n.TaiKhoan == sTaiKhoan && n.MatKhau == sMatKhau);
+            if (kh != null)
+            {
+                ViewBag.ThongBao = "Đăng Nhập Thành Công !";
+                Session["TaiKhoan"] = kh;
+                return View();
+            }
+            ViewBag.ThongBao = "Khong Thanh Cong !";
+            return View();
+        }
+
+	}
+}
